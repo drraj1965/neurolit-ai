f2bd52f3c8904600a158b05f493b1eacd0e242fd
drraj1965 <doctornerves@gmail.com>
2026-04-26 18:33:50 +0400

    Update current version to 1.1.0

--- lib/services/app_version_service.dart
+++ lib/services/app_version_service.dart
@@ -1,6 +1,6 @@
 class AppVersionService {
   static const appName = 'NeuroLit AI';
-  static const currentVersion = '1.0.0';
+  static const currentVersion = '1.1.0';
   static const updateJsonUrl =
       'https://raw.githubusercontent.com/drraj1965/neurolit-ai/main/update.json';
   static const installerFileName = 'NeuroLit_Setup.exe';
